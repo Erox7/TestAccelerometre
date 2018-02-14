2f3edfb1caf804fb20571fb498410aa08651e981
Erox7 <trugove@hotmail.com>
2018-02-14 15:55:01 +0100

    Apartat A fet

--- TestAccelerometre/app/src/main/java/udl/eps/testaccelerometre/TestAccelerometreActivity.java
+++ TestAccelerometre/app/src/main/java/udl/eps/testaccelerometre/TestAccelerometreActivity.java
@@ -10,14 +10,25 @@ import android.os.Bundle;
 
 import android.widget.TextView;
 import android.widget.Toast;
+/*
+BONES PRACTIQUES APLICADES:
+1. Verify sensors before you use them
+2. Unregister sensors listeners.
+3. Don't block the onSensorChanged() method
+4. Choose sensor delays carefully
+5. Avoid using deprecated methods or sensor types
+6. Test your sensor code on a physycal device or use a sensor simulator
 
+El 7é no l'aplique, l'afegim al manifest.
+
+ */
 public class TestAccelerometreActivity extends Activity implements SensorEventListener {
   private SensorManager sensorManager;
   private boolean color = false;
   private TextView view;
   private long lastUpdate;
 
-  //asd
+
 /** Called when the activity is first created. */
 
   @Override
@@ -26,15 +37,25 @@ public class TestAccelerometreActivity extends Activity implements SensorEventLi
 
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
-    view = (TextView) findViewById(R.id.textView);
+    view = (TextView) findViewById(R.id.textView2);
     view.setBackgroundColor(Color.GREEN);
+    TextView bottomView = (TextView) findViewById(R.id.textView3);
+    bottomView.setBackgroundColor(Color.YELLOW);
 
     sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+    TextView center = (TextView) findViewById(R.id.textView);
     if (sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
+      Sensor accelometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
       sensorManager.registerListener(this,
-              sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+              accelometer,
               SensorManager.SENSOR_DELAY_NORMAL);
       // register this class as a listener for the accelerometer sensor
+
+      String capabilities = "Resolution: " + accelometer.getResolution() + "\nPower: " +
+              accelometer.getPower() + "\nMaximum Range: " + accelometer.getMaximumRange();
+      center.setText(getString(R.string.shake) + "\n" + capabilities);
+    }else{
+      center.setText(R.string.noAccel);
     }
       lastUpdate = System.currentTimeMillis();
 
